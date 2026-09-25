e98937c84bab46400cb1f8d4c80d677e607bedce
TASVIA ACADEMY <tasviaacademy@gmail.com>
2026-09-25 16:58:44 +0530

    Add files via upload

---
+++ tasvia-upload-files 2/firebase-config.js
@@ -0,0 +1,10 @@
+// TASVIA Academy – Firebase settings (project: tasvia-academy)
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCzx7PInKPGfWjjCOIv_yLy9deHjb11RIc",
+  authDomain: "tasvia-academy.firebaseapp.com",
+  projectId: "tasvia-academy",
+  storageBucket: "tasvia-academy.firebasestorage.app",
+  messagingSenderId: "689935479467",
+  appId: "1:689935479467:web:ed4575e6ff9714ea177dee",
+  measurementId: "G-MDGS02VGS9"
+};
